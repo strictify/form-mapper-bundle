831873e91e58b13754092f1df90712c6dfbd0a14
WebjobsBiz <mitke013@gmail.com>
2020-10-24 20:32:58 +0200

    New approach

---
+++ src/Store.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Strictify\FormMapper;
+
+/**
+ * @template T
+ */
+class Store
+{
+
+    /** @psalm-var T */
+    private $value;
+
+    /**
+     * @psalm-param T $value
+     */
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @psalm-return T
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
