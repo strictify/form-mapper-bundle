8ebfc9d2f5b74e44f92537f21b1074a3b0af802a
zeljko <mitke013@gmail.com>
2024-04-08 12:15:23 +0200

    Added autowiring feature

--- src/DependencyInjection/StrictifyFormMapperExtension.php
+++ src/DependencyInjection/StrictifyFormMapperExtension.php
@@ -5,15 +5,19 @@ declare(strict_types=1);
 namespace Strictify\FormMapper\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader;
+use Strictify\FormMapper\Service\ComparatorInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 class StrictifyFormMapperExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
+
+        $container->registerForAutoconfiguration(ComparatorInterface::class)
+            ->addTag('strictify_form_mapper.comparator');
     }
 }
